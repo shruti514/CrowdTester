4b0ebc16c5da3ceb99d29b0ab5072e63b5ab6138
Neha-Ramachandra <nralka90@gmail.com>
2015-11-30 07:52:27 -0800

    deleted file.

--- SampleData/Project.js
+++
@@ -1,7 +0,0 @@
-{
-    "providerId":"5656592eed6f9f981b12258c",
-    "providerName":"John",
-    "projectManagerId":"15",
-    "projectName":"Space Innovation",
-    "platforms":["Java","Python"]
-}
