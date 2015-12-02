c6e4e886534ad80b1bdf6ddcbed9e185a87927d5
shruti <shruti.gangras514@gmail.com>
2015-12-02 15:10:46 -0800

    some change

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var _=require('underscore');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3500);
+app.set('port', process.env.PORT || 8080);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
